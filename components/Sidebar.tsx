18bb886a000a59de109eac58e448d2d3d77a7b93
unknown <rehannajam2@gmail.com>
2024-02-06 01:04:26 +0500

    Chore: Used the usePath hook to make adjustments to the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,11 +5,14 @@ import Link from "next/link";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import routes from "@/app/partials/navContent";
+import { usePathname } from "next/navigation";
 
 // CONSTANTS -
 const monserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
 const Sidebar = () => {
+  const pathName = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full text-white">
       <div className="px-3 py-2 flex-1">
@@ -26,7 +29,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathName === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.Icon className={cn("h-5 w-5 mr-3", route.color)} />
